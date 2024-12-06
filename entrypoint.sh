84892e854c825cfe758978b2f67b9e5eb16f6287
simonfita <166803061+simonfita@users.noreply.github.com>
2024-12-06 21:22:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,15 +22,13 @@ fi
 echo "Building $1 for $2"
 mkdir -p $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}
 cd "$GITHUB_WORKSPACE/$5"
-godot --headless -rendering-driver opengl3 --import $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1 -v
+
 echo "Imported - no addon"
 if [ -d "addons/epic-online-services-godot" ]; then
     mkdir -p .godot
     echo "res://addons/epic-online-services-godot/eosg.gdextension" > .godot/extension_list.cfg
 fi
-godot --headless -rendering-driver opengl3 --import $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1 -v
-echo "Imported"
-godot --headless -rendering-driver opengl3 --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1
+godot --headless -rendering-driver opengl3 --${mode} "$2" $GITHUB_WORKSPACE/build/${SubDirectoryLocation:-""}$1 -v
 echo "Build Done"
 
 
